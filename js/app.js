8e3143d299e93f0bbc02fb876f99c1c226e1bbc6
HEDAN13 <hectordaniel1689@gmail.com>
2025-09-27 02:47:27 -0300

    Se agregaron funciones para cargar los elementos desde LocalStorage para poder calcular cantidades y precio manteniendo incluso luego de actualizar la página

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-let comisiones = [];
-let comisionId = 1;
+// Declaro un array para guardar las comisiones y el id para poder ir identificándolas
+let comisiones = JSON.parse(localStorage.getItem("comisiones"));
+let comisionId = parseInt(localStorage.getItem("comisionId"));
 
+if (!comisionId) {
+  comisionId = 1;
+}
+
+if (!comisiones) {
+  comisiones = [];
+}
+
+/**
+ *  Función principal:
+ *  Primero reviso que haya un nombre guardado en el local storage, para que en caso de recargar la página, se vuelvan a cargar los datos.
+ *  Al botón de identificarse, le agrego funcionalidad de activar el formulario de ingreso o salir en caso de que haya un usuario "registrado"
+ */
 function main() {
   const ingreso = document.getElementById("identificarse");
   const nombreGuardado = localStorage.getItem("nombreUsuario");
@@ -10,8 +24,10 @@ function main() {
     cambiarSaludo(nombreGuardado);
     cambiarPrimerParrafo(nombreGuardado);
     mostrarOpciones(nombreGuardado);
+    mostrarSubtotal();
   } else {
     ingreso.textContent = "Ingresar";
+    localStorage.clear();
     cambiarSaludo(null);
     cambiarPrimerParrafo(null);
     mostrarOpciones(null);
@@ -21,45 +37,74 @@ function main() {
     if (ingreso.textContent === "Ingresar") {
       userForm(ingreso);
     } else {
-      localStorage.removeItem("nombreUsuario");
+      localStorage.clear();
       ingreso.textContent = "Ingresar";
       cambiarSaludo(null);
       cambiarPrimerParrafo(null);
       mostrarOpciones(null);
+      window.location.reload(true);
     }
   });
 }
 
+/**
+ * Recibo un nombre o un valor null, para indicar que el valor del nombre se encuentra en el localStorage y muestro las cards con las opciones para seleccionar el tipo de comisión.
+ *
+ * @param {String | null} nombre
+ */
 function mostrarOpciones(nombre) {
   const mostrarDiv = document.getElementById("mostrarCards");
-
+  let cantidad = {};
   if (nombre) {
+    const coms = JSON.parse(localStorage.getItem("comisiones"));
+    if (coms) {
+      cantidad = coms.reduce(
+        (acumulador, com) => {
+          acumulador[com.type] = (acumulador[com.type] || 0) + 1;
+          return acumulador;
+        },
+        {
+          "Ilustración para perfil de redes sociales": 0,
+          "Ilustración medio cuerpo": 0,
+          "Ilustración de mascota": 0,
+        }
+      );
+    } else {
+      cantidad = {
+        "Ilustración para perfil de redes sociales": 0,
+        "Ilustración medio cuerpo": 0,
+        "Ilustración de mascota": 0,
+      };
+    }
+
     mostrarDiv.innerHTML = `
-  <div class="card" style="width: 18rem;">
-    <img src="./assets/imagenPerfil.jpg" class="card-img-top" alt="Ejemplo de imagen de perfil">
-    <div class="card-body">
-      <h5 class="card-title">Imagen de perfil</h5>
-      <p class="card-text"><strong id="precioPerfil">$25 usd</strong> - Digital / Color / 300 dpi y 75 dpi</p>
-      <button class="btn btn-warning" id="agregaPerfil">Agregar</button>
-      <p class="card-text" id="cantidadPerfil">0</p>
+  <div class="row justify-content-evenly pb-3">
+    <div class="card" style="width: 18rem;">
+      <img src="./assets/imagenPerfil.jpg" class="card-img-top" alt="Ejemplo de imagen de perfil">
+      <div class="card-body">
+        <h5 class="card-title">Ilustración para perfil de redes sociales</h5>
+        <p class="card-text"><strong id="precioPerfil">$25 usd</strong> - Digital / Color / 300 dpi y 75 dpi</p>
+        <button class="btn btn-warning" id="agregaPerfil">Agregar</button>
+        <p class="card-text" id="cantidadPerfil">${cantidad["Ilustración para perfil de redes sociales"]}</p>
+      </div>
     </div>
-  </div>
-  <div class="card" style="width: 18rem;">
-    <img src="./assets/medioCuerpo.jpeg" class="card-img-top" alt="Ejemplo de imagen medio cuerpo">
-    <div class="card-body">
-      <h5 class="card-title">Imagen medio cuerpo</h5>
-      <p class="card-text"><strong id="precioMedio">$60 usd</strong> - Digital / Color / 300 dpi</p>
-      <button class="btn btn-warning" id="agregaMedio">Agregar</button>
-      <p class="card-text" id="cantidadMedio">0</p>
+    <div class="card" style="width: 18rem;">
+      <img src="./assets/medioCuerpo.jpeg" class="card-img-top" alt="Ejemplo de imagen medio cuerpo">
+      <div class="card-body">
+        <h5 class="card-title">Ilustración medio cuerpo</h5>
+        <p class="card-text"><strong id="precioMedio">$60 usd</strong> - Digital / Color / 300 dpi</p>
+        <button class="btn btn-warning" id="agregaMedio">Agregar</button>
+        <p class="card-text" id="cantidadMedio">${cantidad["Ilustración medio cuerpo"]}</p>
+      </div>
     </div>
-  </div>
-  <div class="card" style="width: 18rem;">
-    <img src="./assets/mascota.jpeg" class="card-img-top" alt="Ejemplo de mascota">
-    <div class="card-body">
-      <h5 class="card-title">Imagen mascota</h5>
-      <p class="card-text"><strong id="precioMascota">$30 usd</strong> - Digital / Color / 300 dpi</p>
-      <button class="btn btn-warning" id="agregaMascota">Agregar</button>
-      <p class="card-text" id="cantidadMascota">0</p>
+    <div class="card" style="width: 18rem;">
+      <img src="./assets/mascota.jpeg" class="card-img-top" alt="Ejemplo de mascota">
+      <div class="card-body">
+        <h5 class="card-title">Ilustración de mascota</h5>
+        <p class="card-text"><strong id="precioMascota">$30 usd</strong> - Digital / Color / 300 dpi</p>
+        <button class="btn btn-warning" id="agregaMascota">Agregar</button>
+        <p class="card-text" id="cantidadMascota">${cantidad["Ilustración de mascota"]}</p>
+      </div>
     </div>
   </div>
   `;
@@ -80,9 +125,12 @@ function mostrarOpciones(nombre) {
 
         comisiones.push(comision);
         localStorage.setItem("comisiones", JSON.stringify(comisiones));
+        localStorage.setItem(pCantidad.id, parseInt(pCantidad.textContent));
+        localStorage.setItem("comisionId", comisionId);
         let cantidad = parseInt(pCantidad.textContent);
         cantidad++;
         pCantidad.textContent = cantidad;
+        mostrarSubtotal();
       });
     });
   } else {
@@ -90,6 +138,39 @@ function mostrarOpciones(nombre) {
   }
 }
 
+function mostrarSubtotal() {
+  const subtotal = calcularPrecioComisiones();
+  if (subtotal > 0) {
+    const container = document.getElementById("mostrarCards");
+    const subtotalDiv = document.getElementById("subtotal");
+    if (!subtotalDiv) {
+      const subtotalDiv = document.createElement("div");
+      subtotalDiv.classList.add("row");
+      subtotalDiv.setAttribute("id", "subtotal");
+      subtotalDiv.innerHTML = `
+          <div class="col-12">
+            <h3>Subtotal &rarr; $${subtotal}</h3>
+            <button class="bg-info">Ver selección</button>
+          </div>
+      `;
+      container.appendChild(subtotalDiv);
+    } else {
+      subtotalDiv.innerHTML = `
+          <div class="col-12">
+            <h3>Subtotal &rarr; $${subtotal}</h3>
+            <button class="bg-info">Ver selección</button>
+          </div>
+      `;
+    }
+  }
+}
+
+/**
+ * Recibo un el elemento HTML del botón para que una vez registrado el nombre, pueda actualizar el contenido del botón y mostrar "Salir".
+ * Muestro el formulario que solicita el nombre y luego cambio los elementos del DOM de acuerdo con el nombre recibido, el cual además es almacenado en el LocalStorage.
+ *
+ * @param {HTMLElement | null} ingreso
+ */
 function userForm(ingreso) {
   const ingresoDiv = document.getElementById("ingresoForm");
 
@@ -117,6 +198,12 @@ function userForm(ingreso) {
   });
 }
 
+/**
+ * Recibo un string o un null, para cambiar el mensaje de bienvenida al sitio.
+ *
+ * @param {String | null} nombreGuardado
+ */
+
 function cambiarSaludo(nombreGuardado) {
   const saludo = document.getElementById("saludo");
   if (nombreGuardado) {
@@ -125,6 +212,12 @@ function cambiarSaludo(nombreGuardado) {
     saludo.innerText = `Visitante le damos la bienvenida!`;
   }
 }
+
+/**
+ * Recibo el nombre almacenado en localStorage, para cambiar el mensaje del párrafo, de acuerdo a lo recibido.
+ *
+ * @param {String | null} nombreGuardado
+ */
 
 function cambiarPrimerParrafo(nombreGuardado) {
   const parrafo = document.getElementById("primerParrafo");
@@ -136,4 +229,23 @@ function cambiarPrimerParrafo(nombreGuardado) {
   }
 }
 
+/**
+ * Calcula el precio de las comisiones almacenadas en el LocalStorage, para poder mostrar un subtotal de lo seleccionado hasta el momento.
+ *
+ * @returns number
+ */
+
+function calcularPrecioComisiones() {
+  const comisionesGuardadas = JSON.parse(localStorage.getItem("comisiones"));
+  if (comisionesGuardadas) {
+    let total = comisionesGuardadas.reduce(
+      (acumulador, comision) => acumulador + parseInt(comision.price),
+      0
+    );
+    return total;
+  }
+  return 0;
+}
+
+// Llamamos al main
 main();
